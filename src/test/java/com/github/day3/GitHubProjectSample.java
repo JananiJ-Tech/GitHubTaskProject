49780bcba1af95eaab4b9e031c3f8d5ed0768f7b
JananiJ-Tech <jananijcs13@gmail.com>
2025-01-04 20:44:16 +0530

    created 1 class

---
+++ src/test/java/com/github/day3/GitHubProjectSample.java
@@ -0,0 +1,28 @@
+package com.github.day3;
+
+public class GitHubProjectSample {
+	
+	private void tamil() {
+		System.out.println("Tamil");
+
+	}
+	
+	private void english() {
+		System.out.println("English");
+
+	}
+	
+	private void maths() {
+		System.out.println("Maths");
+
+	}
+	
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Sample");
+
+		
+	}
+}
+
