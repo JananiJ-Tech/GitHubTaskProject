ab80a45f67a67d739e47eb0b0161da5797959dd0
JananiJ-Tech <jananijcs13@gmail.com>
2025-01-04 21:03:59 +0530

    added 3methods

--- src/test/java/com/github/day3/GitHubProjectSample.java
+++ src/test/java/com/github/day3/GitHubProjectSample.java
@@ -17,6 +17,20 @@ public class GitHubProjectSample {
 
 	}
 	
+	private void method4() {
+		System.out.println("Method4");
+
+	}
+	
+	private void method5() {
+		System.out.println("Method5");
+
+	}
+	
+	private void method6() {
+		System.out.println("Method6");
+
+	}
 	
 	public static void main(String[] args) {
 	
